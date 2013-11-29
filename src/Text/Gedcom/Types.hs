6748f1d5b98eb52575a3f139275ae35f4d0d8f0d
Ben Edwards <benedwards@google.com>
2013-11-29 17:36:37 +0000

    Added algebra for folding CONT tags in the tree
    Cont tags can now be folded up into record providing they don't branch
    right (which they shouldn't). This transformation is safe even if they
    do though, because unmatched nodes aren't touched.

--- src/Text/Gedcom/Types.hs
+++ src/Text/Gedcom/Types.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Text.Gedcom.Types
   where
 
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
+import Data.Monoid
 import Text.Gedcom.Tree
 
 data XRef = XRef ByteString Integer deriving (Eq,Ord,Show)
@@ -27,3 +31,22 @@ renderAlg (Bin x l r) = (show x):((f r) ++ l)
 
 render :: Show a => Tree a -> String
 render = unlines . cata renderAlg
+
+isCont :: Record -> Bool
+isCont (Record tag _) = tag == "CONT"
+
+collapseContAlg :: Algebra (TreeF Record) (Tree Record)
+collapseContAlg f = 
+    case f of
+        -- cont tag has tree below
+        (Bin x (In (Bin x' l (In Leaf))) r)  | isCont x' -> bin (merge x x') l r
+        -- cont tag is to the right
+        (Bin x l (In (Bin x' l' (In leaf)))) | isCont x' -> bin (merge x x') l l'
+        -- I don't know how to fold anything else
+        _ -> In f
+  where
+    merge (Record tag (B bs)) (Record _ (B bs')) = Record tag (B $ bs <> "\n" <> bs')
+    merge _ _ = error ("collapseContAlg: badtree")
+
+collapseCont :: Tree Record -> Tree Record
+collapseCont = cata collapseContAlg
