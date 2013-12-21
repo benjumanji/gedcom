ff723c4cb0e82e7cae6dded799b46eb88cbb2e45
Ben Edwards <edwards.benj@gmail.com>
2013-12-21 23:01:48 +0000

    Changed parser to use attoparsec instead
    Even though trifecta is a much nicer lib, it doesn't play well with
    streaming, and there is no pipes package for it. Attoparsec is also
    faster, not that it needed to be faster.

--- src/Text/Gedcom/Parser.hs
+++ src/Text/Gedcom/Parser.hs
@@ -1,47 +1,67 @@
 module Text.Gedcom.Parser
+  (gedcom)
   where
 
 import Control.Applicative
+import Data.Attoparsec.ByteString.Char8 
+  ( Parser
+  , char
+  , isDigit
+  , string
+  )
+import qualified Data.Attoparsec.ByteString.Char8 as AT
+import Data.Attoparsec.Combinator
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as C8
+import Data.CharSet (CharSet)
+import qualified Data.CharSet as CS
 import Data.Functor.Foldable (Fix(..))
-import Text.Parser.Char hiding (alphaNum, anyChar, space)
-import Text.Parser.Combinators
-import Text.Parser.Token
-import Text.Trifecta.Combinators
-import Text.Trifecta.Parser
-
 import Text.Gedcom.Types hiding (xref, tag)
+
+eol :: Parser ()
+eol = AT.endOfLine
+
+mkPred :: String -> (Char -> Bool)
+mkPred xs = flip CS.member cs
+  where
+    cs = CS.fromList xs
+
+mkPredInv :: String -> (Char -> Bool)
+mkPredInv xs = flip CS.notMember cs
+  where
+    cs = CS.fromList xs
 
 space :: Parser Char
 space = char ' '
 
-bsFromSet :: String -> Parser ByteString
-bsFromSet = sliced . skipSome . oneOf
+isAlpha = mkPred alphaChars
 
 alpha :: Parser ByteString
-alpha = bsFromSet alphaChars
+alpha = AT.takeWhile isAlpha
 
 alphaChars :: String
 alphaChars = ['A' .. 'Z'] ++ ['a'..'z'] ++ "_"
 
-digitChars :: String
-digitChars = ['0'..'9']
-
 anyChar :: Parser ByteString
-anyChar = (sliced . skipSome $ p)
+anyChar = AT.takeWhile pred
   where
-    p = noneOf "\n"
+    pred = mkPredInv "\n"
+
+data XRefState = Di | Ch
 
 xref :: Parser XRef
-xref = XRef <$ char '@' <*> p <* char '@'
+xref = XRef <$ char '@' <*> AT.scan Ch g <* char '@'
   where
-    p = sliced (alpha *> natural)
+    g Ch x | isAlpha x = Just Ch
+           | isDigit x = Just Di
+           | otherwise = Nothing
+    g Di x = if isDigit x then Just Di else Nothing
 
 payload :: Parser Payload
-payload = optional space *> ((X <$> xref) <|> (B <$> anyChar) <|> pure Null)
+payload = option Null (space *> ((X <$> xref) <|> (B <$> anyChar)))
 
 record :: Int -> Parser Records
-record n = f <$ np <* space <*> alpha <*> payload <* newline <*> rs
+record n = f <$ np <* space <*> alpha <*> payload <* eol <*> rs
   where
     np = nat n
     rs = many . record $ n + 1
@@ -51,7 +71,7 @@ zero :: Parser Char
 zero = char '0'
 
 nat :: Int -> Parser ()
-nat n = () <$ string (show n)
+nat n = () <$ string (C8.pack $ show n)
 
 gedcom :: Parser Gedcom
-gedcom = Gedcom <$ zero <* space <*> xref <* space <*> alpha <* newline <*> some (record 1)
+gedcom = Gedcom <$ zero <* space <*> xref <* space <*> alpha <* eol <*> some (record 1)
